afe251cda34cfd20be52b8d510e58683a831b9ea
Milas Javellana <milas.d.javellana@gmail.com>
2024-12-11 12:52:46 -0600

    completed part 1

--- scripts/2024/1.py
+++ scripts/2024/1.py
@@ -1,9 +1,18 @@
 from timer import time_it
 def main():
-    lines = [line.strip() for line in open('inputs/2024/1.txt').readlines()]
+    lines = [line.strip().split('   ') for line in open('inputs/2024/1.txt').readlines()]
+    list1, list2 = list(map(list, zip(*lines)))
+    list1 = list(map(int,list1))
+    list2 = list(map(int,list2))
     @time_it
     def part1():
-        pass
+        diff = 0
+        print(list1)
+        list1.sort()
+        list2.sort()
+        for i in range(len(list1)):
+            diff += abs(list1[i]-list2[i])
+        return diff
     @time_it
     def part2():
         pass
