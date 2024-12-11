17364e045dd340e16a9970a2e75ed8df00e134bc
Milas Javellana <milas.d.javellana@gmail.com>
2024-12-11 15:25:28 -0600

    completed part 2. Elf 2402 may rest

--- scripts/2024/2.py
+++ scripts/2024/2.py
@@ -21,7 +21,36 @@ def main():
 
     @time_it
     def part2():
-        pass
+        #whatever I have done to deserve this, I am sorry
+        safe = 0
+        for line in lines:
+            goodSoFar = True
+            if line not in [sorted(line), sorted(line)[::-1]]:
+                goodSoFar = False
+            prev = line[0]
+            for num in line[1:]:
+                if abs(num-prev) not in range(1,4):
+                    goodSoFar = False
+                    break
+            if goodSoFar:
+                safe += 1
+                continue
+            for problem in range(len(line)):
+                ostrich = True
+                line_ = line[:problem] + line[problem+1:] #so glad I remembered to do that
+                print(line, line_)
+                if line_ not in [sorted(line_), sorted(line_)[::-1]]:
+                    continue
+                prev_ = line_[0]
+                for num_ in line_[1:]:
+                    if abs(num_-prev_) not in range(1,4):
+                        ostrich = False
+                        break
+                    prev_ = num_
+                if ostrich:
+                    safe += 1
+                    break
+        return safe
     print(f'Part 1: {part1()}')
     print(f'Part 2: {part2()}')
 if __name__ == '__main__':
