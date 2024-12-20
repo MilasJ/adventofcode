e14cf4f12ed3c316b5a00f46c848c87e18b619b4
Milas Javellana <milas.d.javellana@gmail.com>
2024-12-20 17:19:21 -0600

    changed run.py to grab input if not found

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
+from os import system
 import sys
 try:
-    _,year,day = sys.argv
+    year,day = [int(num) for num in sys.argv[-2:]]
     exec(open(f'scripts/{year}/{day}.py').read())
 except ValueError:
     print('Use exactly two values: make sure you use both year and day')
-# except FileNotFoundError:
-#     print(_path)
-#     print('Make sure to enter:\n\t\'py run.py [year] [day]\'')
+except FileNotFoundError:
+    if int(day-1) in range(25):
+        system(f'python newfile.py {year} {day}')
+        exec(open(f'scripts/{year}/{day}.py').read())
+    else:
+        print('Make sure to enter:\n\t\'py run.py [year] [day]\'')
