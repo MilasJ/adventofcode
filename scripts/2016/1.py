291049b5cbb3b594ea16d458e0b7a07744d31ed0
Milas Javellana <milas.d.javellana@gmail.com>
2024-12-20 17:33:47 -0600

    completed part 2. Elf 1601 may rest

--- scripts/2016/1.py
+++ scripts/2016/1.py
@@ -18,7 +18,28 @@ def main():
         return abs(pos.real) + abs(pos.imag)
     @time_it
     def part2():
-        pass
+        line = lines[0]
+        pos = 0
+        face = 1j
+        locs = []
+        done = False
+        for step in line:
+            direction = step[0]
+            length = int(step[1:])
+            match direction:
+                case 'L':
+                    face *= 1j
+                case 'R':
+                    face *= -1j
+            for _ in range(length):
+                pos += face
+                if pos in locs:
+                    done = True
+                    break
+                locs.append(pos)
+            if done:
+                break
+        return abs(pos.real) + abs(pos.imag)
     print(f'Part 1: {part1()}')
     print(f'Part 2: {part2()}')
 if __name__ == '__main__':
