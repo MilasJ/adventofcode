930a855c601e43ee722190c901f9bf59000fdb2a
Milas Javellana <milas.d.javellana@gmail.com>
2025-12-01 20:56:11 -0600

    added new cookie

--- newfile.py
+++ newfile.py
@@ -6,7 +6,7 @@ cookie = {
     # '_ga':'GA1.2.1763632613.1670886531',
     # '_gid':'GA1.2.1160750930.1674516883',
     # '_gat':'1',
-    'session':'53616c7465645f5f3629edbaeb85a2b8c366f7c09adb785325acf4473fb53689f90437954735668c6ae0aa7af0ecf6d38da5c9db1222a6b1e66f6fd6d0b75825'}
+    'session':'53616c7465645f5f7b6fd25f5edad251c334a5967a626b48bef22b3436dbbd755cd9c30b9ed088ce97e4dacb203991c9018fc2cc9db860b8cefe2e722996fb7f'}
 for folder in ('inputs','scripts'):
     if not os.path.isdir(f'/{folder}/{year}'):
         try:
