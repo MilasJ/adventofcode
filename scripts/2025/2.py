81ea505c72f96b18cb1f3503146693d3d51dd0da
Milas Javellana <milas.d.javellana@gmail.com>
2025-12-03 16:11:25 -0600

    completed part 2. Elf 2502 may rest

--- scripts/2025/2.py
+++ scripts/2025/2.py
@@ -1,4 +1,5 @@
 from timer import time_it
+import textwrap
 def main():
     lines = [line.strip() for line in open('inputs/2025/2.txt').readlines()]
     ranges = [[int(num) for num in range.split("-")] for range in lines[0].split(",")]
@@ -11,13 +12,28 @@ def main():
                 digits = len(numstring)
                 half = int(digits/2)
                 if numstring[:half] == numstring[half:]:
-                    idsum += int(numstring)
+                    idsum += num
         return idsum
                 
 
     @time_it
-    def part2():
-        pass
+    def part2(): #  Thirty seconds to run. Oy...
+        idsum = 0
+        for start, end in ranges:
+            for num in range(start, end+1):
+                numstring = str(num)
+                digits = len(numstring)
+                for factor in range(1, digits):
+                    if digits % factor:
+                        continue
+                    groups = textwrap.wrap(numstring, factor)
+                    if all(group == groups[0] for group in groups):
+                        idsum += num
+                        break
+        return idsum
+                    
+
+
     print(f'Part 1: {part1()}')
     print(f'Part 2: {part2()}')
 if __name__ == '__main__':
