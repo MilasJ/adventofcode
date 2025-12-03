44efbf63ac7682ff88ccd102f5b00d94ba93eddf
Milas Javellana <milas.d.javellana@gmail.com>
2025-12-02 18:24:57 -0600

    completed part 2. Elf 2501 may rest

--- scripts/2025/1.py
+++ scripts/2025/1.py
@@ -16,7 +16,17 @@ def main():
         return counter
     @time_it
     def part2():
-        pass
+        dial = 50
+        counter = 0
+        directions = {"L":-1, "R":1}
+        for line in lines:
+            direction = directions[line[0]]
+            for _ in range(int(line[1:])):
+                dial += direction
+                dial = dial%100
+                if dial == 0:
+                    counter += 1
+        return counter
     print(f'Part 1: {part1()}')
     print(f'Part 2: {part2()}')
 if __name__ == '__main__':
